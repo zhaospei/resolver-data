db8399b0c2c93a744d0c7768e2c4392b44089004
Zhao-Linux <dungbuit1k28@gmail.com>
2022-11-22 22:49:01 +0700

    change layout file json

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,20 @@
 #include <chrono>
 using namespace std;
 // Name of file
-const string NAME = "data-submissions";
+const string NAME = "contest";
 // Number of data values
-const int NDATA = 100;
+const int NDATA = 1000;
 // Number of users
-const int NUSER = 10;
+const int NUSER = 12;
 // Number of problems
 const int NPROBLEM = 6;
 // Max of Point
 const int POINT_MAX = 100;
 // Time distance
 const int TIME_DISTANCE = 20;
+// Range of subtasks
+const int MIN_SUBTASK = 3;
+const int MAX_SUBTASK = 6;
 // Time 
 int cur_time = 0;
 
@@ -26,32 +29,95 @@ long long Rand(long long l, long long h) {
     assert(l <= h);
     return l + rd() * 1LL * rd() % (h - l + 1);
 }
-long long RandPoint(int point_zero, int point_ac) {
+long long RandPoint(int point_zero, int point_ac, int point_max) {
     int point_part = 100 - point_ac - point_zero;
     int value = Rand(0, 100);
     if (value <= point_zero) {
         return 0;
     } else {
         if (value <= point_ac + point_zero) {
-            return POINT_MAX;
+            return point_max;
         } else {
-            return Rand(1, 99);
+            return Rand(1, point_max - 1);
         }
     }
 }
+int nsubtask[NPROBLEM + 1][7];
 
 int main() {
     srand(time(NULL));
-    freopen((NAME + ".txt").c_str(), "w", stdout);
-    cout <<"    \"solutions\":{\n";
-    for (int id = 1; id <= NDATA; id++) {
-        cout << "       \"" << id << "\":{" << "\n";
-        cout << "         \"user_id\":" << Rand(1, NUSER) << ",\n";
-        cout << "         \"problem_index\":" << Rand(1, NPROBLEM) << ",\n";
-        cout << "         \"point\":" << RandPoint(20, 20) << ",\n";
-        cur_time = cur_time + Rand(0, TIME_DISTANCE);
-        cout << "         \"submitted_seconds\":" << cur_time << "\n";
-        cout << "       },\n";
+    freopen((NAME + ".json").c_str(), "w", stdout);
+    cout << "{\n";
+    cout <<"  \"problems\":{\n";
+    for (int id = 1; id <= NPROBLEM; id++) {
+        cout << "    \"" << id << "\":{" << "\n";
+        int subtasks = Rand(MIN_SUBTASK, MAX_SUBTASK);
+        int cur_point = 100;
+        for (int subtask = 1; subtask <= subtasks; subtask++) {
+            int point;
+            if (subtask < subtasks) {
+                point = Rand(10, cur_point - 10 * (subtasks - subtask + 1));
+                nsubtask[id][subtask] = point;
+                cur_point -= point;
+                cout << "      \"" << subtask << "\":" << point << ",\n";
+            } else {
+                point = cur_point;
+                nsubtask[id][subtask] = point;
+                cout << "      \"" << subtask << "\":" << point << "\n";
+            }
+        }
+        if (id < NPROBLEM) {
+            cout << "    },\n";
+        } else {
+            cout << "    }\n"; 
+        }
     }
+    cout << "  },\n";
+    cout <<"  \"solutions\":{\n";
+    for (int id = 1; id <= NDATA; id++) {
+        int curproblem = Rand(1, NPROBLEM);
+        cout << "    \"" << id << "\":{" << "\n";
+        cout << "      \"user_id\":" << Rand(1, NUSER) << ",\n";
+        cout << "      \"problem_index\":" << curproblem << ",\n";
+        cout << "      \"points\":{\n";
+        int nsub = 0;
+        for (int i = 1; i <= 6; i++) {
+            if (nsubtask[curproblem][i] == 0) {
+                break;
+            }
+            nsub = i; 
+        }
+        for (int i = 1; i <= nsub; i++) {
+            int subpoint = RandPoint(30 - (6 - i) * 5, 30 - (i - 1) * 5, nsubtask[curproblem][i]);
+            if (i < nsub) {
+                cout << "        \""<< i << "\":" << subpoint << ",\n";
+            } else {
+                cout << "        \""<< i << "\":" << subpoint << "\n";
+            }
+        }
+        cout << "      },\n";
+        cur_time = cur_time + Rand(0, TIME_DISTANCE);
+        cout << "      \"submitted_seconds\":" << cur_time << "\n";
+        if (id < NDATA) {
+            cout << "    },\n";
+        } else {
+            cout << "    }\n";
+        }
+    }
+    cout <<"  },\n";
+    cout <<"  \"users\":{\n";
+    for (int id = 1; id <= NUSER; id++) {
+        cout << "    \"" << id << "\":{" << "\n";
+        cout << "      \"username\":\"user" << id << "\",\n";
+        cout << "      \"name\":\"Nguyen Van A" << char(97 + id - 1) << "\",\n";
+        cout << "      \"school\":\"School " << char(65 + id - 1) << "\"\n";
+        if (id < NUSER) {
+            cout << "    },\n";
+        } else {
+            cout << "    }\n"; 
+        }
+    }
+    cout <<"  }\n";
+    cout << "}\n";
     return 0;
 }
